8660026009ff4fa17f0533b693ac495b82170b76
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2022-07-15 08:57:26 -0400

    Added Keyboard
    They keyboard for the game has been added

---
+++ main.js
@@ -0,0 +1,45 @@
+const tiles = document.querySelector('.tiles')
+const keyboard = document.querySelector('.keyboard')
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '«',
+]
+
+function handleClick() {
+    console.log('clicked')
+}
+
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    buttonElement.setAttribute('id', key)
+    buttonElement.addEventListener('click', () => handleClick(key))
+    keyboard.append(buttonElement)
+})
