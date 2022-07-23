552e6cc055397fd25f8a51a51c6a6278d34359ea
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2022-07-23 17:40:30 -0400

    new game
    edited html, css, and js files

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ Word Battle is a two player game where one opponent tries to guess a randomly ch
 ## Description
 The goal is to guess the 5 letter word chosen by the computer, with only 5 tries. Player 1 will go first. Using the keyboard on the screen, Player 1 will guess and enter a 5 letter word. If any of the letters match the computer's selected word, the tile will light up yellow. If the letter matches the selected word AND is in the right placement, the tile will light up green. Player 2 will make the next guess based on the tile colors provided by Player 1. If a player guesses the selected word they win! If the word is not guessed at all then the computer wins. 
 
+## Built With
+
+* JavaScript
+* HTML
+* CSS
+
+
 ## Authors
 Laura Minaya
 
--- main.js
+++ main.js
@@ -1,97 +1,84 @@
-const words = [
-    'ABYSS',
-    'ABHOR',
-    'ABIDE',
-    'ABODE',
-    'CABIN',
-    'CABLE',
-    'DANCE',
-    'DAISY',
-    'DAIRY',
-    'FABLE',
-    'FAIRY',
-    'FAITH',
-    'HABIT',
-    'HEDGE',
-    'HARSH',
-    'JOINT',
-    'JEWEL',
-    'JAZZY',
-    'SHINE',
-    'SCOPE',
-    'TREAT',
-    'JUDGE',
-    'KNIFE',
-    'MEDAL',
-    'MERCY',
-    'NASAL',
-    'NOVEL',
-    'OASIS',
-    'OMEGA',
-    'OUNCE',
-    'PAINT',
-    'PATIO',
-    'PAUSE',
-    'PIANO',
-    'PILOT',
-    'PULSE',
-    'QUERY',
-    'QUOTE',
-    'QUEST',
-    'RADIO',
-    'RAPID',
-    'REBEL',
-    'ROBOT',
-    'ROUTE',
-    'RURAL',
-    'SATIN',
-    'SEWER',
-    'SHELF',
-    'SPACE',
-    'SWING',
-    'TRIAL',
-    'TRIBE',
-    'TWIST',
-    'TOWER',
-    'USUAL',
-    'UNITY',
-    'VAGUE',
-    'VOCAL',
-    'VAPOR',
-    'VENUE',
-    'VINYL',
-    'VIVID',
-    'VIRUS',
-    'WAGON',
-    'WHEAT',
-    'WRIST',
-    'WRATH',
-    'YACHT',
-    'YOUNG',
-    'YOUTH'
+let myQuestions = [
+    {
+        question: 'This Order of the Phoenix member is a metamorphmagus, an auror, a hufflpeuff, and is married to a werewolf. They prefer to go by this nickname',
+        correctAnswer: 'TONKS'
+    },
+    {
+        question: 'One of the fabled Deathly Hallows.. this item rendered its wearer invisible',
+        correctAnswer: 'CLOAK'
+    },
+    {
+        question: 'Name this Weasley family owl with very poor eyesight',
+        correctAnswer: 'ERROL'
+    }, 
+    {
+        question: 'Name the incantation for the summoning charm',
+        correctAnswer: 'ACCIO'
+    }, 
+    {
+        question: 'Out of the three unforgiveable curses, this one causes instant death. Name the first word of this incantation',
+        correctAnswer: 'AVADA'
+    },
+    {
+        question: 'This professor at Hogwarts served as Potions Master and head of Sytherin House',
+        correctAnswer: 'SNAPE'
+    }, 
+    {
+        question: 'This coveted magical profession works as a member of an elite unit of highly-trained, specialist officers. They protect the wizarding world from practitioners of the Dark Arts',
+        correctAnswer: 'AUROR'
+    }, 
+    {
+        question: 'A magical plant that can constrict or stangle anything in its surrounding environment. Two words, name the second word. HINT: It is deadly fun but will sulk in the sun.',
+        correctAnswer: 'SNARE'
+    }, 
+    {
+        question: 'Charm that illuminates the tip of the casters wand',
+        correctAnswer: 'LUMOS'
+    },
+    {
+        question: 'Popular wizarding pub. Located at the entrance to Diagon Alley. Two words. Name the first.',
+        correctAnswer: 'LEAKY'
+    },
+    {
+        question: 'Half brother to Hagrid, short by giant standards...',
+        correctAnswer: 'GRAWP'
+    },
+    {
+        question: 'Name for a non-magical person born to at least one magical parent.',
+        correctAnswer: 'SQUIB'
+    }
 ]
 
-const numOfGuesses = 5
-const guessesRemaining = numOfGuesses
-let currentGuess = []
-let nextLetter = 0
-let rightGuessString = words[Math.floor(Math.random() * words.length)]
-
-function readyBoard() {
-    let board = document.querySelector('.gameBoard')
-
-    for(let i = 0; i < numOfGuesses; i++) {
-        let row = document.createElement('div')
-        row.className = 'letter-row'
-
-        for(let j = 0; j < 5; j++) {
-            let tile = document.createElement('div')
-            tile.className = 'letter-tile'
-            row.appendChild(tile)
-        }
-
-        board.appendChild(row)
+function showQuestions() {
+    for(let i = 0; i < myQuestions.length; i++){
+        document.getElementById('msg').innerHTML = (JSON.stringify(myQuestions[0].question))
     }
 }
 
-readyBoard()
+function keyboardEvents() {
+    document.body.onkeydown = (e) => {
+        const key = e.key
+
+        if(key === 'Enter'){
+            
+        }
+
+        if(key === 'Backspace'){}
+
+        if(isLetter(key)){}
+    }
+
+    // updateTileRow()
+}
+
+// function updateTileRow()
+
+// function isLetter() {}
+    
+// function showScore() {}
+
+// function startGame() {}
+
+console.log((JSON.stringify(myQuestions[0].correctAnswer)))
+
+showQuestions()
