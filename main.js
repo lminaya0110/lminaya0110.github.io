0a9c5182e4c3a2734df67f04f1867786c3bc4a35
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2022-07-25 21:17:11 -0400

    Changed Game to Quiz
    Html, css and js file updated

--- main.js
+++ main.js
@@ -1,84 +1,189 @@
-let myQuestions = [
-    {
-        question: 'This Order of the Phoenix member is a metamorphmagus, an auror, a hufflpeuff, and is married to a werewolf. They prefer to go by this nickname',
-        correctAnswer: 'TONKS'
-    },
-    {
-        question: 'One of the fabled Deathly Hallows.. this item rendered its wearer invisible',
-        correctAnswer: 'CLOAK'
-    },
-    {
-        question: 'Name this Weasley family owl with very poor eyesight',
-        correctAnswer: 'ERROL'
-    }, 
-    {
-        question: 'Name the incantation for the summoning charm',
-        correctAnswer: 'ACCIO'
-    }, 
-    {
-        question: 'Out of the three unforgiveable curses, this one causes instant death. Name the first word of this incantation',
-        correctAnswer: 'AVADA'
-    },
-    {
-        question: 'This professor at Hogwarts served as Potions Master and head of Sytherin House',
-        correctAnswer: 'SNAPE'
-    }, 
-    {
-        question: 'This coveted magical profession works as a member of an elite unit of highly-trained, specialist officers. They protect the wizarding world from practitioners of the Dark Arts',
-        correctAnswer: 'AUROR'
-    }, 
-    {
-        question: 'A magical plant that can constrict or stangle anything in its surrounding environment. Two words, name the second word. HINT: It is deadly fun but will sulk in the sun.',
-        correctAnswer: 'SNARE'
-    }, 
-    {
-        question: 'Charm that illuminates the tip of the casters wand',
-        correctAnswer: 'LUMOS'
-    },
-    {
-        question: 'Popular wizarding pub. Located at the entrance to Diagon Alley. Two words. Name the first.',
-        correctAnswer: 'LEAKY'
-    },
-    {
-        question: 'Half brother to Hagrid, short by giant standards...',
-        correctAnswer: 'GRAWP'
-    },
-    {
-        question: 'Name for a non-magical person born to at least one magical parent.',
-        correctAnswer: 'SQUIB'
-    }
+const Questions = [{
+    id: 0,
+    q: "This Order of the Phoenix member is a metamorphmagus, an auror, and a hufflpeuff. They prefer to go by this nickname",
+    a: [{ text: "Prongs", isCorrect: false },
+        { text: "Padfoot", isCorrect: false },
+        { text: "Tonks", isCorrect: true },
+        { text: "Moody", isCorrect: false }
+    ]
+
+},
+{
+    id: 1,
+    q: "One of the fabled Deathly Hallows.. this item rendered its wearer invisible",
+    a: [{ text: "Horcrux", isCorrect: false, isSelected: false },
+        { text: "Stone", isCorrect: false },
+        { text: "Wand", isCorrect: false },
+        { text: "Cloak", isCorrect: true }
+    ]
+
+},
+{
+    id: 2,
+    q: "Name this Weasley family owl with very poor eyesight",
+    a: [{ text: "Errol", isCorrect: true },
+        { text: "Dobby", isCorrect: false },
+        { text: "Scabbers", isCorrect: false },
+        { text: "Hedwig", isCorrect: false }
+    ]
+
+},
+{
+    id: 3,
+    q: "Name the incantation for the summoning charm",
+    a: [{ text: "Aberto", isCorrect: false },
+        { text: "Accio", isCorrect: true },
+        { text: "Crucio", isCorrect: false },
+        { text: "Lumos", isCorrect: false }
+    ]
+
+},
+{
+    id: 4,
+    q: "Name the first word of the unforgiveable curse that causes instant death",
+    a: [{ text: "Kedavra", isCorrect: false },
+        { text: "Ascendio", isCorrect: false },
+        { text: "Avifors", isCorrect: false },
+        { text: "Avada", isCorrect: true }
+    ]
+
+},
+{
+    id: 5,
+    q: "Hogwarts potion master and head of Sytherin House",
+    a: [{ text: "Snape", isCorrect: true },
+        { text: "Pomfrey", isCorrect: false },
+        { text: "Sprout", isCorrect: false },
+        { text: "Quirrell", isCorrect: false }
+    ]
+
+},
+{
+    id: 6,
+    q: "This coveted profession protects the wizarding world from practitioners of the Dark Arts",
+    a: [{ text: "Curse-breaker", isCorrect: false },
+        { text: "Minister", isCorrect: false },
+        { text: "Auror", isCorrect: true },
+        { text: "Headmaster", isCorrect: false }
+    ]
+
+},
+{
+    id: 7,
+    q: "A magical plant that constricts and strangles. It is deadly fun but will sulk in the sun.'",
+    a: [{ text: "Basilisk", isCorrect: false },
+        { text: "Mandrakes", isCorrect: false },
+        { text: "Devil's Snare", isCorrect: true },
+        { text: "Gillyweed", isCorrect: false }
+    ]
+
+},
+{
+    id: 8,
+    q: "Charm that illuminates the tip of the casters wand",
+    a: [{ text: "Portus", isCorrect: false },
+        { text: "Lumos", isCorrect: true },
+        { text: "Bombarda", isCorrect: false },
+        { text: "Episkey", isCorrect: false }
+    ]
+
+}
+
+
 ]
 
-function showQuestions() {
-    for(let i = 0; i < myQuestions.length; i++){
-        document.getElementById('msg').innerHTML = (JSON.stringify(myQuestions[0].question))
-    }
-}
+var start = true;
+function iterate(id) {
+  
+    var result = document.getElementsByClassName("result");
+    result[0].innerText = "";
+  
+ 
+    const question = document.getElementById("question");
+    question.innerText = Questions[id].q;
+  
+    const op1 = document.getElementById('op1');
+    const op2 = document.getElementById('op2');
+    const op3 = document.getElementById('op3');
+    const op4 = document.getElementById('op4');
 
-function keyboardEvents() {
-    document.body.onkeydown = (e) => {
-        const key = e.key
+        op1.innerText = Questions[id].a[0].text;
+        op2.innerText = Questions[id].a[1].text;
+        op3.innerText = Questions[id].a[2].text;
+        op4.innerText = Questions[id].a[3].text;
+      
 
-        if(key === 'Enter'){
-            
+        op1.value = Questions[id].a[0].isCorrect;
+        op2.value = Questions[id].a[1].isCorrect;
+        op3.value = Questions[id].a[2].isCorrect;
+        op4.value = Questions[id].a[3].isCorrect;
+      
+        var selected = "";
+
+    op1.addEventListener("click", () => {
+        op1.style.backgroundColor = "#7A7265";
+        op2.style.backgroundColor = "#170312";
+        op3.style.backgroundColor = "#170312";
+        op4.style.backgroundColor = "#170312";
+        selected = op1.value;
+    })
+  
+
+    op2.addEventListener("click", () => {
+        op1.style.backgroundColor = "#170312";
+        op2.style.backgroundColor = "#7A7265";
+        op3.style.backgroundColor = "#170312";
+        op4.style.backgroundColor = "#170312";
+        selected = op2.value;
+    })
+
+
+        op3.addEventListener("click", () => {
+            op1.style.backgroundColor = "#170312";
+            op2.style.backgroundColor = "#170312";
+            op3.style.backgroundColor = "#7A7265";
+            op4.style.backgroundColor = "#170312";
+            selected = op3.value;
+        })
+      
+
+        op4.addEventListener("click", () => {
+            op1.style.backgroundColor = "#170312";
+            op2.style.backgroundColor = "#170312";
+            op3.style.backgroundColor = "#170312";
+            op4.style.backgroundColor = "#7A7265";
+            selected = op4.value;
+        })
+
+
+    const evaluate = document.getElementsByClassName("evaluate");
+  
+
+    evaluate[0].addEventListener("click", () => {
+        if (selected == "true") {
+            result[0].innerHTML = "True";
+            result[0].style.color = "#81C14B";
+        } else {
+            result[0].innerHTML = "False";
+            result[0].style.color = "#93032E";
         }
-
-        if(key === 'Backspace'){}
-
-        if(isLetter(key)){}
-    }
-
-    // updateTileRow()
+    })
 }
 
-// function updateTileRow()
+if (start) {
+    iterate("0");
+}
+  
 
-// function isLetter() {}
-    
-// function showScore() {}
-
-// function startGame() {}
-
-console.log((JSON.stringify(myQuestions[0].correctAnswer)))
-
-showQuestions()
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+  
+next.addEventListener("click", () => {
+    start = false;
+    if (id < 8) {
+        id++;
+        iterate(id);
+        console.log(id);
+    }
+  
+})
